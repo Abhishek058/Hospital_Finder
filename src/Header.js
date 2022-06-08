f4f5fc09b541cdf8447fd03debdbe8938983cd90
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-06-08 19:32:29 +0530

    header section added with all styling

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex flex-row p-5">
+      <div className="imgdiv flex flex-row mx-7">
+        <img className="w-7 h-7 mx-2" src="/images/hosp9.png" alt="logo" />
+        <h1 className="text-xl font-bold">IndiHospitality</h1>
+      </div>
+      <div className="text-center linkdiv space-x-7 w-9/12 font-medium text-lg">
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Services</a>
+        <a href="#">Pharmacy</a>
+      </div>
+      <div className="LiBtn mx-10 w-36 text-center font-medium self-center">
+        LogIn
+      </div>
+    </div>
+  );
+}
+
+export default Header;
