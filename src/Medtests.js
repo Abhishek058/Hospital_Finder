844fd8521e2ca1d302f73a3222ff1692cb114aa4
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-06-09 19:01:59 +0530

    covid testing section completed with all styling and other styling bug fixed

--- src/Medtests.js
+++ src/Medtests.js
@@ -2,41 +2,44 @@ import React from "react";
 
 function Medtests() {
   return (
-    <div className="Medtests text-center ">
-      <h1 className="text-3xl font-bold mt-10 mb-10">
+    <div className="Medtests text-center flex flex-col">
+      <h1 className="text-3xl font-bold mt-10 mb-5">
         COVID-19 Test and Vaccination
       </h1>
       <div className="tests flex flex-row mt-20">
         <div className="tst1 flex flex-col w-max mx-20">
           <div className="tst2 p-1 flex justify-center items-center rounded-full cursor-pointer">
-            <img className="w-40 rounded-full" src="/images/hosp3.jpg" alt="" />
+            <img className="w-40 rounded-full" src="/images/cov1.jpg" alt="" />
           </div>
           <h1 className="font-medium mt-7">RAPID TEST</h1>
         </div>
         <div className="tst1 flex flex-col w-max mx-20">
           <div className="tst2 p-1 flex justify-center items-center rounded-full cursor-pointer">
-            <img className="w-40 rounded-full" src="/images/hosp3.jpg" alt="" />
+            <img className="w-40 rounded-full" src="/images/cov2.jpg" alt="" />
           </div>
           <h1 className="font-medium mt-7">PCR TEST</h1>
         </div>
         <div className="tst1 flex flex-col w-max mx-20">
           <div className="tst2 p-1 flex justify-center items-center rounded-full cursor-pointer">
-            <img className="w-40 rounded-full" src="/images/hosp3.jpg" alt="" />
+            <img className="w-40 rounded-full" src="/images/cov3.jpg" alt="" />
           </div>
           <h1 className="font-medium mt-7">DRIVE THRU TEST</h1>
         </div>
         <div className="tst1 flex flex-col w-max mx-20">
           <div className="tst2 p-1 flex justify-center items-center rounded-full cursor-pointer">
-            <img className="w-40 rounded-full" src="/images/hosp3.jpg" alt="" />
+            <img className="w-40 rounded-full" src="/images/cov4.jpg" alt="" />
           </div>
           <h1 className="font-medium mt-7">VACCINATION</h1>
         </div>
         <div className="tst1 flex flex-col w-max mx-20">
           <div className="tst2 p-1 flex justify-center items-center rounded-full cursor-pointer">
-            <img className="w-40 rounded-full" src="/images/hosp3.jpg" alt="" />
+            <img className="w-40 rounded-full" src="/images/cov5.jpg" alt="" />
           </div>
           <h1 className="font-medium mt-7">HOME TEST</h1>
         </div>
+      </div>
+      <div className="rgbtn my-12 w-36 text-center font-medium self-center">
+        Register
       </div>
     </div>
   );
