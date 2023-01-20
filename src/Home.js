08833c6f418ce556bd480133e57615ae1efb7813
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-20 23:51:40 +0530

    responsiveness updated

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,14 @@ import React from "react";
 function Home() {
   return (
     <div className="Home flex flex-row">
-      <div className="leftdiv w-2/3 p-16">
+      <div className="leftdiv flex flex-col w-2/3 p-16">
         <h1 className="text-7xl font-bold p-5">
           A heritage in care.
           <br />A reputation in excellence.
         </h1>
         <p className="m-7 text-lg text-gray-500">
-          IndiHospitality provides excellent servicers by prioritizing the
-          security and security of patients. Finding nearest hospital for you.
+          IndiHospitality provides excellent services by prioritizing the
+          safety and security of patients. Finding nearest hospital for you.
         </p>
         <div className="playvid flex my-10 mx-7">
           <img className="playimg w-10 h-10" src="/images/play.png" alt="" />
