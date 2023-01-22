e0a76c2f17e154edf6aaf9ce2a94951f5d9dc180
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-22 18:28:33 +0530

    login basic tags added

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,24 @@ import { Link } from "react-router-dom";
 function Login() {
   return (
     <div className="Login">
-      LogIn
+      <div class="container">
+        <h1>Log In</h1>
+        <form>
+          <div class="form-control">
+            <input type="text" required />
+            <label>Username</label>
+          </div>
+          <div class="form-control">
+            <input type="password" required />
+            <label>Password</label>
+          </div>
+          <button class="btn">Submit</button>
+          <p class="text">
+            Don't have an account?
+            <a href="#">Create Account</a>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
