87b21224ce0c7112d7ea502dc9cafc5224735b5b
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-25 22:53:45 +0530

    handle on submit function created

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 function Login() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const json = await response.json();
+    console.log(json);
+  };
   return (
     <div className="Login flex flex-col justify-center items-center h-screen">
       <div className="container w-96 h-3/5 bg-black bg-opacity-50  rounded-md text-white flex flex-col justify-center items-center p-5">
         <h1 className="text-4xl font-bold mb-9">Log In</h1>
-        <form className="flex flex-col justify-center items-center">
+        <form
+          className="flex flex-col justify-center items-center"
+          onSubmit={handleSubmit}
+        >
           <div className="form-control  mt-5 p-4 w-full">
             <input
               type="text"
               className="w-full"
+              id = "email"
+              name="email"
               placeholder="Username"
               required
             />
@@ -18,6 +34,8 @@ function Login() {
             <input
               type="password"
               className="w-full"
+              id = "password"
+              name="password"
               placeholder="Password"
               required
             />
